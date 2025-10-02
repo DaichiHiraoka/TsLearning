ac893b917877f6f28873998e199444b748bd6f6c
Daichi Hiraoka <ok230195@oks.iput.ac.jp>
2025-10-02 16:31:47 +0900

    classの基礎まで学習

--- src/test.ts
+++ src/test.ts
@@ -23,8 +23,81 @@ export const test =() =>{
         }
         return arr.reverse();
     }
-    console.log(reverseArray(hobbies,true));*/
+    console.log(reverseArray(hobbies,true));
 
-    
+    const user = {
+        userID:1,
+        name:'mike',
+        age:25,
+        email:'mike@gmail.com',
+        isActive:true,
+    }
+
+    interface UserProps {
+        userID: number;
+        name: string;
+        age: number;
+        email: string;
+        
+    }
+    interface UserProps {
+        isActive: boolean;
+    }//同じinterfaceを複数記述するとマージされる
+
+    interface AdminUser extends UserProps {
+        role:string; //"admin", "leader", "manager"
+    }
+
+    type UserProps = {
+        userID: number;
+        name: string;
+        age: number;
+        email: string;
+        isActive: boolean;
+    }
+    const createaccount = (accountInfo: UserProps): void => {
+        //function call
+    }
+    createaccount({
+        userID:1,
+        name:'mike',
+        age:25,
+        email:'mike@gmail.com',
+        isActive:true,
+    });*/
+
+    class User {
+        userID: number;
+        name: string;
+        age: number;
+        email: string;
+        isActive: boolean;
+
+        constructor(userID: number, name: string, age: number, email: string, isActive: boolean) {
+            this.userID = userID;
+            this.name = name;
+            this.age = age;
+            this.email = email;
+            this.isActive = isActive;
+
+            this.getProfile();
+        }
+        private getProfile(): string {
+            return `Name: ${this.name}, Age: ${this.age}`;
+        }
+        public sayHello(): void {
+            console.log(`Hello, ${this.name}`);
+        }
+    }
+    const user = new User(1, 'mike', 25, "mike@gmail.com", true);
+    class Admin extends User {
+        role: string;
+        constructor(userID: number, name: string, age: number, email: string, isActive: boolean, role: string) {
+            super(userID, name, age, email, isActive);
+            this.role = role;
+        }
+    }
+    const adminUser = new Admin(1, 'mike', 25, "mike@gmail.com", true, "admin");
+    adminUser.sayHello();//親クラスのメソッド 
+
 }
-

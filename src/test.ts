8d308aeb0271b42f77eed551cd30c9989aa09bd0
Daichi Hiraoka <ok230195@oks.iput.ac.jp>
2025-10-02 12:25:29 +0900

    初回push 環境構築済み

---
+++ src/test.ts
@@ -0,0 +1,10 @@
+export const test =() =>{
+    console.log('test');
+
+    const name: string = 'mike'; //string
+    const age: number = 25; //number
+    const isStudent: boolean = false; //boolean
+    const hobbies: string[] = ['reading', 'gaming', 'coding']; //array of strings
+
+    console.log(name);
+}

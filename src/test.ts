fd38871ecdcae72702e3dbd26da9dfedb6e3137d
Daichi Hiraoka <ok230195@oks.iput.ac.jp>
2025-10-02 17:22:56 +0900

    ジェネリクス、null合体演算子、オプショナルチェーンまで 学習終了

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 export const test =() =>{
-    /*console.log('test');
-
+    
+    /*//型注釈の基礎
     const name: string = 'mike'; //string
     const age: number = 25; //number
     const isStudent: boolean = false; //boolean
@@ -17,6 +17,8 @@ export const test =() =>{
     const value: string | number | null = 'Hello World'; //union
     console.log(name);
 
+
+    //関数の型注釈
     const reverseArray = (arr: string[], hasTom?: boolean): string[] => {
         if(hasTom){
             arr.unshift('tom');
@@ -66,7 +68,9 @@ export const test =() =>{
         isActive:true,
     });*/
 
-    class User {
+
+    //クラスの基礎
+    /*class User {
         userID: number;
         name: string;
         age: number;
@@ -98,6 +102,47 @@ export const test =() =>{
         }
     }
     const adminUser = new Admin(1, 'mike', 25, "mike@gmail.com", true, "admin");
-    adminUser.sayHello();//親クラスのメソッド 
+    adminUser.sayHello();//親クラスのメソッド */
 
-}
+
+    /*//ジェネリクス
+    const foo=<T>(arg: T,): T => arg;//どの型も許容するが、anyとは違い型チェックされる
+
+    function func<T>(x: T): T | number {
+        return typeof x === "string" ? x.length : 0;
+    }
+    //入力した型に関わらずnumberを返すことを許容したい場合はこのようにする
+    foo<string>('hello');*/
+
+
+    /*//Null合体演算子
+    const fetchUserData = async()=>{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1',{
+            useId:1,
+        });
+        return response.data.response;
+    }
+    const isActive:boolean = fetchUserData() ?? false;//nullまたはundefinedの場合falseを代入
+
+    //if(isActive){...}*/
+
+
+    /*//optional chaining
+    const fetchUserData = async()=>{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1',{
+            useId:1,
+        });
+        return response.data;
+    }
+    const useInfoData = fetchUserData();
+        // {
+            // id: 1, 
+            // name: "Leanne Graham",
+            // email: "leanne.graham@example.com"
+            //  address: {
+            //      street: "Kulas Light",
+            //      city: "Gwenborough"
+            //  },
+        //}
+    const userCity = useInfoData?.address?.city;//addressやcityが存在しない場合undefinedを返す*/
+}//他との型の整合性で動作テストできない場合はとりあえずany指定して後から直すと良い

28aef4f8c287a3c9ab04eace36556704b52cddb1
Daichi Hiraoka <ok230195@oks.iput.ac.jp>
2025-10-02 12:58:01 +0900

    データ型の種類まで学習

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,22 @@
 export const test =() =>{
-    console.log('test');
+    /*console.log('test');
 
     const name: string = 'mike'; //string
     const age: number = 25; //number
     const isStudent: boolean = false; //boolean
     const hobbies: string[] = ['reading', 'gaming', 'coding']; //array of strings
-
+    const address: object = { //object
+        city: 'Los Angeles',
+        state: 'CA'
+    };//object
+    const today: Date = new Date(); //Date
+    const none: null = null; //null
+    const notDefined: undefined = undefined; //undefined
+    const random: any = 'Hello World'; //any
+    const gender: 'male' | 'female' = 'male'; //union
+    const value: string | number | null = 'Hello World'; //union
     console.log(name);
+
+    
 }
+
